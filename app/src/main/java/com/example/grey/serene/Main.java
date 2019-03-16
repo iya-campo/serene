578252a92a27e6b89e3f3626f7c5e2c5aeaef2f6
Ilyanna Frances V. Campo <ilyannacampo@gmail.com>
2019-03-17 00:05:15 +0800

    Test

--- app/src/main/java/com/example/grey/serene/Main.java
+++ app/src/main/java/com/example/grey/serene/Main.java
@@ -1,44 +1,13 @@
 package com.example.grey.serene;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
-
-  private TextView mTextMessage;
-
-  private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-    = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-      switch (item.getItemId()) {
-        case R.id.navigation_home:
-          mTextMessage.setText("Home");
-          return true;
-        case R.id.navigation_journal:
-          mTextMessage.setText("Journal");
-          return true;
-        case R.id.navigation_insights:
-          mTextMessage.setText("Insights");
-          return true;
-      }
-      return false;
-    }
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    mTextMessage = (TextView) findViewById(R.id.message);
-    BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
   }
-
 }
