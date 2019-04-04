45e2d0391b3a70653583952832df50372c276cf2
Ilyanna Frances V. Campo <ilyannacampo@gmail.com>
2019-04-04 14:25:28 +0800

    timepicker and listview

--- app/src/main/java/com/example/grey/serene/AccountRegisterNotifs.java
+++ app/src/main/java/com/example/grey/serene/AccountRegisterNotifs.java
@@ -1,31 +1,37 @@
 package com.example.grey.serene;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DatabaseReference;
 
-public class AccountRegisterNotifs extends AppCompatActivity {
+import java.util.Calendar;
 
-    Button yes, no;
-    String notif;
-    String alarmTitle;
-    String alarm;
-    String userName;
-    String userEmail;
-    String userPassword;
-    String userNickname;
-    int userAge;
-    EditText alarmName;
-    DatabaseReference ref;
-    Spinner time, day;
-    Users user;
-    long maxid;
+public class AccountRegisterNotifs extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+  int hour, minute;
+
+  Button timeButton;
+  Button yes, no;
+
+  String notif;
+  String alarmTitle;
+  String alarm;
+  String userName;
+  String userEmail;
+  String userPassword;
+  String userNickname;
+  int userAge;
+  EditText alarmName;
+  DatabaseReference ref;
+  Users user;
+  long maxid;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,9 @@ public class AccountRegisterNotifs extends AppCompatActivity {
 
     getSupportActionBar().hide();
 
+    timeButton = (Button) findViewById(R.id.timeButton);
     Button next2Button = (Button) findViewById(R.id.next2Button);
+
     /**Commented for testing purposes**/
     /*
     alarmName = (EditText) findViewById(R.id.alarmTitleField);
@@ -94,6 +102,17 @@ public class AccountRegisterNotifs extends AppCompatActivity {
       }
     });
     */
+    timeButton.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View view){
+        Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR);
+        minute = c.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(AccountRegisterNotifs.this,
+          (TimePickerDialog.OnTimeSetListener) AccountRegisterNotifs.this, hour, minute, false);
+        timePickerDialog.show();
+      }
+    });
     next2Button.setOnClickListener((new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -112,6 +131,15 @@ public class AccountRegisterNotifs extends AppCompatActivity {
       }
     }));
   }
+
+  @Override
+  public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+    int hour = hourOfDay % 12;
+    if(hour == 0)
+      hour = 12;
+    timeButton.setText(String.format("%02d:%02d %s", hour, minute, hourOfDay < 12 ? "am" : "pm"));
+  }
+
     /*
     public void addFruits(View view) {
       EditText userNickname=(EditText) findViewById(R.id.answer1Field);
--- app/src/main/java/com/example/grey/serene/MainInsights.java
+++ app/src/main/java/com/example/grey/serene/MainInsights.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -14,18 +18,59 @@ import android.widget.TextView;
  */
 public class MainInsights extends Fragment {
 
-  TextView textView;
+  private ExpandableListView listView;
+  private ExpandableListAdapter listAdapter;
+  private List<String> listDataHeader;
+  private HashMap<String, List<String>> listHash;
+
+  Boolean menuState;
+  //Button insightButton;
 
   public MainInsights() {
     // Required empty public constructor
   }
 
-
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_main_insights, container, false);
+    View view = inflater.inflate(R.layout.fragment_main_insights, container, false);
+
+    listView = (ExpandableListView) view.findViewById(R.id.insightsMenu);
+    initData();
+    listAdapter = new ExpandableListAdapter(this.getActivity(), listDataHeader, listHash);
+    listView.setAdapter(listAdapter);
+
+    //insightButton = (Button) view.findViewById(R.id.insightButton);
+    menuState = false;
+/*
+    insightButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        if(menuState == false){
+          menuState = true;
+        } else {
+          menuState = false;
+        }
+        Toast.makeText(getActivity(), "" + menuState, Toast.LENGTH_SHORT).show();
+      }
+    });
+*/
+    return view;
+  }
+
+  private void initData() {
+    listDataHeader = new ArrayList<>();
+    listHash = new HashMap<>();
+
+    listDataHeader.add("Menu");
+
+    List<String> menu1 = new ArrayList<>();
+    menu1.add("Serene Insights");
+    menu1.add("Saved Insights");
+
+    listHash.put(listDataHeader.get(0), menu1);
   }
 
   public void addArticles(){
