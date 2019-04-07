546b1f15a7cd36ef03258001d5f0d5362cde58db
Iya_Campo <ilyannacampo@gmail.com>
2019-04-07 15:07:37 +0800

    calendar and rounded bg

--- app/src/main/java/com/example/grey/serene/JournalEntry.java
+++ app/src/main/java/com/example/grey/serene/JournalEntry.java
@@ -4,17 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class JournalEntry extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class JournalEntry extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_journal_entry);
 
     getSupportActionBar().hide();
+
+    Spinner spinner = (Spinner) findViewById(R.id.sleepSpinner);
+    List<String> hoursOfSleep = new ArrayList<String>();
+    hoursOfSleep.add("01");
+    hoursOfSleep.add("02");
+    hoursOfSleep.add("03");
+    hoursOfSleep.add("04");
+    hoursOfSleep.add("05");
+    hoursOfSleep.add("06");
+    hoursOfSleep.add("07");
+    hoursOfSleep.add("08");
+    hoursOfSleep.add("09");
+    hoursOfSleep.add("10");
+    hoursOfSleep.add("11");
+    hoursOfSleep.add("12");
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_style, hoursOfSleep);
+    spinner.setAdapter(adapter);
 
     //Entry Header Buttons
     Button backButton = (Button) findViewById(R.id.backButton);
@@ -94,5 +117,16 @@ public class JournalEntry extends AppCompatActivity {
   public void finish(){
     super.finish();
     overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+  }
+
+  @Override
+  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    String text = parent.getItemAtPosition(position).toString();
+    Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onNothingSelected(AdapterView<?> parent) {
+
   }
 }
